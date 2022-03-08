f9e0222342d48123ebf06644d15d8f08029c9c63
anthonny <anthonnyerasrivera@gmal.com>
2022-03-07 18:06:24 -0600

    desafio desaparecer footer

--- app.js
+++ app.js
@@ -2,9 +2,27 @@
 const carrito = document.getElementById("carrito")
 const template = document.getElementById("template")
 const fragment = document.createDocumentFragment() //evitar reflow
+const footer = document.getElementById("footer")
+const templateFooter = document.getElementById("templateFooter")
 
-const botones = document.querySelectorAll(".card .btn")
 
+document.addEventListener("click", (e) => {
+    // console.log(e.target.matches(".card .btn-outline-primary"))
+    if (e.target.matches(".card .btn-outline-primary")) {
+        //console.log("ejecutar agregar al carro")
+        agregarAlCarrito(e)
+    }
+
+    // console.log(e.target.matches(".list-group-item .btn-success"))
+
+    if (e.target.matches("#carrito .list-group-item .btn-success")) {
+        btnAumentar(e)
+    }
+
+    if (e.target.matches("#carrito .list-group-item .btn-danger")) {
+        btnDisminuir(e)
+    }
+})
 // prueba para saber si estan bien ligados
 // console.log(carrito)
 // console.log(template)
@@ -12,16 +30,18 @@ const botones = document.querySelectorAll(".card .btn")
 // console.log(btn)
 
 // objeto
-const carritoObjeto = []
+let carritoObjeto = []
 // con la "e.target" nos traemos la informacion del boton que se digito
 const agregarAlCarrito = (e) => {
-    console.log(e.target.dataset.fruta)
+    // console.log(e.target.dataset.fruta)
 
     // objeto
+    //Aqui se capturan los data
     const producto = {
         titulo: e.target.dataset.fruta,
         id: e.target.dataset.fruta,
-        cantidad: 1
+        cantidad: 1,
+        precio: parseInt(e.target.dataset.precio),
     }
 
     const indice = carritoObjeto.findIndex(
@@ -37,8 +57,8 @@ const agregarAlCarrito = (e) => {
     }
 
     console.log(carritoObjeto)
-    pintarCarrito(carritoObjeto)
-
+    pintarCarrito()
+    pintarFooter()
 }
 
 {
@@ -58,21 +78,59 @@ const agregarAlCarrito = (e) => {
 }
 
 // agregarle producto en la linea 55 no es tan necesario ya que al final se esta usando carritoObjeto en la linea 56, es opcional
-const pintarCarrito = (array) => {
+const pintarCarrito = () => {
 
     carrito.textContent = ""
 
-    array.forEach((item) => {
-        const clone = template.content.firstElementChild.cloneNode(true)
-        clone.querySelector(".lead").textContent = item.titulo
+    carritoObjeto.forEach((item) => {
+        const clone = template.content.cloneNode(true)
+        clone.querySelector(".text-white .lead").textContent = item.titulo
         clone.querySelector(".badge").textContent = item.cantidad
+        clone.querySelector("div .lead span").textContent = item.precio * item.cantidad;
 
+        clone.querySelector(".btn-danger").dataset.id = item.id
+        clone.querySelector(".btn-success").dataset.id = item.id
+        // NOTA: el fragment mayormente se utiliza en ciclos, como carritoObjeto
         fragment.appendChild(clone)
     })
 
     carrito.appendChild(fragment)
 }
 
+const pintarFooter = () => {
+    footer.textContent = ""
 
+    const total = carritoObjeto.reduce(
+        (acc, current) => acc + current.cantidad * current.precio, 0
+    )
+
+    const clone = templateFooter.content.cloneNode(true)
+
+    clone.querySelector("span").textContent = total
+    footer.appendChild(clone)
+}
+const btnAumentar = (e) => {
+    // console.log("me diste click", e.target.dataset.id)
+    carritoObjeto = carritoObjeto.map(item => {
+        if (item.id === e.target.dataset.id) {
+            item.cantidad++
+        }
+        return item
+    })
+    pintarCarrito()
+}
+
+const btnDisminuir = (e) => {
+    carritoObjeto = carritoObjeto.filter(item => {
+        if (item.id === e.target.dataset.id) {
+            if (item.cantidad > 0) {
+                item.cantidad--
+                if (item.cantidad === 0) return //si el item es igual a 0 se borra el elemento
+                return item
+            }
+        } else { return item }
+    })
+    pintarCarrito()
+}
 // detectamos los botones y recorre los botones
-botones.forEach((btn) => btn.addEventListener("click", agregarAlCarrito))
+// botones.forEach((btn) => btn.addEventListener("click", agregarAlCarrito))
